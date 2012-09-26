2eea4e45b73f2f6e99fc80430a66b4def684af68
Jon Fagerholm <jon.fagerholm@gmail.com>
2012-09-27 01:16:42 +0200

    Created project

---
+++ beertest-webapp/src/main/java/no/jaf/beertest/App.java
@@ -0,0 +1,13 @@
+package no.jaf.beertest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
