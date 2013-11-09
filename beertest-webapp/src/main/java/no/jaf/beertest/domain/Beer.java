cdababffd96db3137c48e9dda2379fc8aa5b7a3c
Jon André Fagerholm <jon.fagerholm@gmail.com>
2013-11-09 15:49:35 +0100

    removed equals and hashcode

--- beertest-webapp/src/main/java/no/jaf/beertest/domain/Beer.java
+++ beertest-webapp/src/main/java/no/jaf/beertest/domain/Beer.java
@@ -48,34 +48,4 @@ public class Beer implements Serializable {
     public void setName(String name) {
         this.name = name;
     }
-
-    @Override
-    public int hashCode() {
-        int hash = 3;
-        hash = 29 * hash + (this.id != null ? this.id.hashCode() : 0);
-        hash = 29 * hash + (this.producer != null ? this.producer.hashCode() : 0);
-        hash = 29 * hash + (this.name != null ? this.name.hashCode() : 0);
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        final Beer other = (Beer) obj;
-        if (this.id != other.id && (this.id == null || !this.id.equals(other.id))) {
-            return false;
-        }
-        if ((this.producer == null) ? (other.producer != null) : !this.producer.equals(other.producer)) {
-            return false;
-        }
-        if ((this.name == null) ? (other.name != null) : !this.name.equals(other.name)) {
-            return false;
-        }
-        return true;
-    }    
 }
--- beertest-webapp/src/main/java/no/jaf/beertest/domain/Person.java
+++ beertest-webapp/src/main/java/no/jaf/beertest/domain/Person.java
@@ -32,32 +32,4 @@ public class Person implements Serializable {
     public void setName(String name) {
         this.name = name;
     }
-
-    @Override
-    public int hashCode() {
-        int hash = 3;
-        hash = 89 * hash + (this.id != null ? this.id.hashCode() : 0);
-        hash = 89 * hash + (this.name != null ? this.name.hashCode() : 0);
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        final Person other = (Person) obj;
-        if (this.id != other.id && (this.id == null || !this.id.equals(other.id))) {
-            return false;
-        }
-        if ((this.name == null) ? (other.name != null) : !this.name.equals(other.name)) {
-            return false;
-        }
-        return true;
-    }
-    
-    
 }
