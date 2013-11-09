181731eedd6ec807670d1d3576dfda75852e0755
Jon Fagerholm <jon.fagerholm@accenture.com>
2013-11-09 15:43:05 +0100

    added sonar support

---
+++ beertest-webapp/target/classes/no/jaf/beertest/domain/Beer_.java
@@ -0,0 +1,15 @@
+package no.jaf.beertest.domain;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.0.v20130507-rNA", date="2013-11-09T15:40:09")
+@StaticMetamodel(Beer.class)
+public class Beer_ { 
+
+    public static volatile SingularAttribute<Beer, Long> id;
+    public static volatile SingularAttribute<Beer, String> name;
+    public static volatile SingularAttribute<Beer, String> producer;
+
+}
---
+++ beertest-webapp/target/generated-classes/cobertura/no/jaf/beertest/domain/Person_.java
@@ -0,0 +1,14 @@
+package no.jaf.beertest.domain;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.0.v20130507-rNA", date="2013-11-09T15:40:09")
+@StaticMetamodel(Person.class)
+public class Person_ { 
+
+    public static volatile SingularAttribute<Person, Long> id;
+    public static volatile SingularAttribute<Person, String> name;
+
+}
